c6d58edb215a73c652973e31e2b42297c6d8c76a
puja <pujarana1796@gmail.com>
2022-04-18 13:01:29 +0530

    sixth commit

--- employee/Program.cs
+++ employee/Program.cs
@@ -93,18 +93,53 @@ namespace employee
 
         //fifth commit
 
+        //public const int IS_PART_TIME = 1;
+        //public const int IS_FULL_TIME = 2;
+        //public const int EMP_RATE_PER_HOUR = 20;
+        //public const int NUM_OF_WORKING_DAYS = 2;
+
+        //static void Main(string[] args)
+        //{
+        //    int empHrs = 0;
+        //    int empWage = 0;
+        //    int totalEmpWage = 0;
+        //    for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+        //    {
+        //        Random random = new Random();
+        //        int empCheck = random.Next(0, 3);
+        //        switch (empCheck)
+        //        {
+        //            case IS_PART_TIME:
+        //                empHrs = 4;
+        //                break;
+        //            case IS_FULL_TIME:
+        //                empHrs = 8;
+        //                break;
+        //            default:
+        //                empHrs = 0;
+        //                break;
+        //        }
+        //        empWage = empHrs * EMP_RATE_PER_HOUR;
+        //        totalEmpWage += empWage;
+        //        Console.WriteLine("Emp Wage :" + empWage);
+        //    }
+        //    Console.WriteLine("Total Emp Wage :" + totalEmpWage);
+        //}
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HRS_IN_MONTH = 10;
+
 
         static void Main(string[] args)
         {
             int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int totalempHrs = 0;
+            int totalWorkingDays = 0;
+        while(totalempHrs<= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -119,11 +154,12 @@ namespace employee
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage :" + empWage);
+               
+                totalempHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "empHrs : " + empHrs);
             }
-            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
+            int totalempWage = totalempHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("totalempWage :" + totalempWage);;
         }
 
     }
