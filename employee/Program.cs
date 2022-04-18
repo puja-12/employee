7477d1ee618ed562afd7abbdd5c31f8f8903fc6b
puja <pujarana1796@gmail.com>
2022-04-18 16:22:48 +0530

    third commit

--- employee/Program.cs
+++ employee/Program.cs
@@ -6,42 +6,7 @@ namespace employee
     {
         static void Main(string[] args)
         {
-            //first commit
-
-            //int ispresent = 1;
-            // Random random = new Random();
-
-            //int empCheck = random.Next(0, 2);
-            //if (empCheck == ispresent)
-            //{
-            //   Console.WriteLine("employee is present");
-
-            //}
-            //else
-            // {
-            //   Console.WriteLine("employee is absent");
-            // }
-
-            //second commit
-
-            //int IS_FULL_TIME = 1;
-            //int EMP_RATE_PER_HOUR = 20;
-            //int empHrs = 0;
-            //int empWage = 0;
-            //Random random = new Random();
-            //computation
-            //int empCheck = random.Next(0, 2);
-            //if (empCheck == IS_FULL_TIME)
-            //{
-            //    empHrs = 8;
-            //}
-            //else
-            //{
-            //    empHrs = 0;
-            //        }
-            //empWage = empHrs * EMP_RATE_PER_HOUR;
-            //Console.WriteLine("Emp Wage :" + empWage);
-
+            
             //third commit
             int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
