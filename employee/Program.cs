95e3c3a92a5744e179f345109b77332d06c1b69f
puja <pujarana1796@gmail.com>
2022-04-18 11:27:58 +0530

    second commit

--- employee/Program.cs
+++ employee/Program.cs
@@ -6,19 +6,37 @@ namespace employee
     {
         static void Main(string[] args)
         {
-            int ispresent = 1;
+            //int ispresent = 1;
+            // Random random = new Random();
+
+            //int empCheck = random.Next(0, 2);
+            //if (empCheck == ispresent)
+            //{
+            //   Console.WriteLine("employee is present");
+
+            //}
+            //else
+            // {
+            //   Console.WriteLine("employee is absent");
+            // }
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-
+            //computation
             int empCheck = random.Next(0, 2);
-            if (empCheck == ispresent)
+            if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("employee is present");
-
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("employee is absent");
-            }
+                empHrs = 0;
+                    }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage :" + empWage);
+
         }
     }
 }
