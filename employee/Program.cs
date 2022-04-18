f9844f6ebfa8f4d62b610b2b693d8b7fc7fbe842
puja <pujarana1796@gmail.com>
2022-04-18 16:28:49 +0530

    fourth commit

--- employee/Program.cs
+++ employee/Program.cs
@@ -5,70 +5,8 @@ namespace employee
     public class Employee
     {
 
-       // static void Main(string[] args)
-       // {
-       //     Console.WriteLine("Welcome to Employee wage computation program");
-      //  }
-            //first commit
 
-            //int ispresent = 1;
-            // Random random = new Random();
-
-            //int empCheck = random.Next(0, 2);
-            //if (empCheck == ispresent)
-            //{
-            //   Console.WriteLine("employee is present");
-
-            //}
-            //else
-            // {
-            //   Console.WriteLine("employee is absent");
-            // }
-
-            //second commit
-
-            //int IS_FULL_TIME = 1;
-            //int EMP_RATE_PER_HOUR = 20;
-            //int empHrs = 0;
-            //int empWage = 0;
-            //Random random = new Random();
-            //computation
-            //int empCheck = random.Next(0, 2);
-            //if (empCheck == IS_FULL_TIME)
-            //{
-            //    empHrs = 8;
-            //}
-            //else
-            //{
-            //    empHrs = 0;
-            //        }
-            //empWage = empHrs * EMP_RATE_PER_HOUR;
-            //Console.WriteLine("Emp Wage :" + empWage);
-
-            //third commit
-            //int IS_FULL_TIME = 2;
-            //int EMP_RATE_PER_HOUR = 20;
-            //int IS_PART_TIME = 1;
-            //int empHrs = 0;
-            //int empWage = 0;
-            //Random random = new Random();
-            //int empCheck = random.Next(0, 3);
-            //if (empCheck == IS_PART_TIME)
-            //{
-            //    empHrs = 4;
-            //}
-            //else if (empCheck == IS_FULL_TIME)
-            //{
-            //    empHrs = 8;
-            //}
-            //else
-            //{
-            //    empHrs = 0;
-            //}
-            //empWage = empHrs * EMP_RATE_PER_HOUR;
-            //Console.WriteLine("Emp Wage :" + empWage);
-
-            //fourth comment
+            //fourth commit
             public const int IS_PART_TIME = 1;
        public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
